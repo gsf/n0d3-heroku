f210c57706dedc826eb96239a2d9a56bb2782f6f
Gabriel Farrell <g@grrawr.com>
2012-12-27 22:36:33 -0500

    A couple few updates

--- heroku.js
+++ heroku.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
-module.exports = function heroku (room, log) {
+module.exports = function heroku (client, bot) {
   // Create a local webserver
   http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
@@ -9,8 +9,9 @@ module.exports = function heroku (room, log) {
 
   // Ping yourself every 5 minutes to keep dyno alive
   setInterval(function(){
+    bot.log('Pinging myself');
     http.get(process.env.HEROKU, function(res) {})
   }, 300000)
 
-  console.log('Self Awareness server running');
+  bot.log('Self-awareness server running');
 };
